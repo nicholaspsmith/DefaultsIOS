51a61530c4bef4649f183eff6e63c35f765bf49c
Nicholas Smith <npsmith1990@gmail.com>
2015-12-17 20:28:59 -0600

    Create 3 buttons with action outlets

--- defaults/ViewController.swift
+++ defaults/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func red(sender: AnyObject) {
+    }
 
+    @IBAction func yellow(sender: AnyObject) {
+    }
+    
+    @IBAction func blue(sender: AnyObject) {
+    }
 }
 
